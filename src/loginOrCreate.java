2b7a304f1b71a2f9c9e9055fa7f2154d230812b4
t00650151 <sanyamg37@gmail.com>
2022-10-17 13:13:00 -0700

    made gui for login/create user

---
+++ src/loginOrCreate.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class loginOrCreate {
+    private JPanel loginOrCreatePanel;
+    private JButton haveAnAccountLoginButton;
+    private JButton createAccountButton;
+}

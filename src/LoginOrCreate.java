21b9c0acfaf74039401314440b0011ec65ab6e1e
t00650151 <sanyamg37@gmail.com>
2022-10-17 13:51:42 -0700

    Made working GUI

---
+++ src/LoginOrCreate.java
@@ -0,0 +1,34 @@
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginOrCreate extends JFrame{
+    private JPanel LoginOrCreatePanel;
+    private JButton haveAnAccountLoginButton;
+    private JButton createAccountButton;
+
+    public LoginOrCreate() {
+        setContentPane(LoginOrCreatePanel);
+        setVisible(true);
+        setSize(500,500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        haveAnAccountLoginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               // LoginFrame loginFrame = new LoginFrame();
+                //loginFrame.setVisible(true);
+            }
+        });
+        createAccountButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args){
+        new LoginOrCreate();
+    }
+}
